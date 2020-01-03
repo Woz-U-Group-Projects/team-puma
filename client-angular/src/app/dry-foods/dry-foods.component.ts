cec7a31b7d62e65ef8e550e88df9a8d1ef8c84d4
AdamBlaire1 <Adam.Blaire@gmail.com>
2020-01-02 16:37:14 -0800

    added to the dry foods page

--- client-angular/src/app/dry-foods/dry-foods.component.ts
+++ client-angular/src/app/dry-foods/dry-foods.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dryfood } from '../models/dryfood';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dry-foods',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dry-foods.component.css']
 })
 export class DryFoodsComponent implements OnInit {
-
-  constructor() { }
-
+  public dryfoods: Dryfood[];
+  private dryfoodsRoute = 'http://localhost:3000/dryfoods';
+  constructor(private http: HttpClient) { }
+  getDryfood() {
+    this.http.get<Dryfood[]>(this.dryfoodsRoute).subscribe(dryfoods => {
+      this.dryfoods = dryfoods;
+      console.log('Dryfood', this.dryfoods);
+    });
+  }
   ngOnInit() {
+    this.getDryfood();
   }
 
 }
